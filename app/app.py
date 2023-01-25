612cd8fe28d37c092f6dc4fb339fd147d7562932
Fabio <fabio.silva@fotosensores.com>
2023-01-25 19:21:43 -0300

    return early e comentarios

--- app/app.py
+++ app/app.py
@@ -5,13 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import time
 
 def setup_browser():
 
     options = webdriver.ChromeOptions()
 
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
 
     service = Service(ChromeDriverManager().install())
 
@@ -27,58 +26,60 @@ def main():
     #iniciando dict dos dados
     data = dict()
     
+    # acessando a pagina
     r = requests.get(url)
     
-    if r.status_code == 200:
+    # return early caso houver algum problema para acessar a URL
+    if r.status_code != 200:
+        print('URL não acessivel')
+        return
+    
+    # selecionando tabela a ser trabalhada
+    content = r.content
         
-        content = r.content
+    soup = BeautifulSoup(content, 'html.parser')
         
-        soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find('tbody', {'class': 'lister-list'})
         
-        table = soup.find('tbody', {'class': 'lister-list'})
+    all_movies = table.find_all('tr')
         
-        all_movies = table.find_all('tr')
-        
-        for row in all_movies:
+    for row in all_movies:
             
-            # pegando valores da linha
-            movie_data = row.find_all('td')
+        # pegando valores da linha
+        movie_data = row.find_all('td')
             
-            # ranking do filme pegando o index da linha + 1
-            movie_rank = all_movies.index(row) + 1
+        # ranking do filme pegando o index da linha + 1
+        movie_rank = all_movies.index(row) + 1
             
-            # nome do filme
-            movie_name = movie_data[1].find('a').text
+        # nome do filme
+        movie_name = movie_data[1].find('a').text
             
-            # avaliação imdb
-            movie_imdb_rating = movie_data[2].text.replace('\n', '')
+        # avaliação imdb
+        movie_imdb_rating = movie_data[2].text.replace('\n', '')
             
-            # pegando o scr do poster do filme
-            movie_img_scr = movie_data[0].find('img')['src']
+        # pegando o scr do poster do filme
+        movie_img_scr = movie_data[0].find('img')['src']
             
-            # criando um dict com os dados do filme
-            data[movie_name] = {
+        # criando um dict com os dados do filme
+        data[movie_name] = {
                 
-                'movie_rank': movie_rank,
+            'movie_rank': movie_rank,
                 
-                'movie_name': movie_name,
+            'movie_name': movie_name,
                 
-                'movie_imdb_rating': movie_imdb_rating,
+            'movie_imdb_rating': movie_imdb_rating,
                 
-                'movie_img_scr': movie_img_scr
+            'movie_img_scr': movie_img_scr
                 
-            }
+        }
         
-        print("Todos os dados recebido!")
+
+    print("Todos os dados recebido!")
         
-        # Criação do DataFrame com os dados do dicionario    
-        df = pd.DataFrame(data).transpose().set_index('movie_rank')
+    # Criação do DataFrame com os dados do dicionario    
+    df = pd.DataFrame(data).transpose().set_index('movie_rank')
             
-        print(df)
-        
-    else:
-        
-        print('Url não acessivel!')     
+    print(df)
 
     
 
