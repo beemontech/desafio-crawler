f4a28eab9ad7c14325310906ccd0dd846c605b41
Ailson da Cruz <ailsoncgt@gmail.com>
2023-11-05 10:07:20 -0300

    chore: libs & improves

--- app/app.py
+++ app/app.py
@@ -4,20 +4,18 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from . import views
 
-CRAWLER_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "crawler")
-
 app = Flask(__name__)
+
 app.config["MONGO_URI"] = os.environ["MONGODB_URI"]
 mongo = PyMongo(app)
-db = mongo.db
+# db = mongo.db
 
 # celery config
 app.config.from_mapping(
     CELERY=dict(
-        broker_url="redis://redis:6379/0", result_backend="redis://redis:6379/0"
+        broker_url=os.environ["REDIS_URI"], result_backend=os.environ["REDIS_URI"]
     )
 )
-
 celery_app = make_celery(app)
 
 app.add_url_rule("/", view_func=views.index)
